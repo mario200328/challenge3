e2469fcb136ecad4d64833d9561213874f88fba6
Mario Balderas <mario000james@gmail.com>
2021-05-30 11:09:19 -0500

    edit JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-var number = "0123456789";
-var symbol = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+var alphaUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var alphaLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var number = ['0','1','2','3','4','5','6','7','8','9'];
+var symbol = ['!','#','$','%','&','\','(','+','-','/','<','>','?'];
 
 
 //  beginning instructions
@@ -26,19 +26,19 @@ function generatePassword() {
  
  else{
      if(confirm("Would you like your password to contain upper case letters?")){
-         Array.prototype.push.apply(allChars, alphaUpper);
+        Array.apply (allChars, alphaUpper);
      }
 
      if(confirm("Would you like your password to contain lower case letters?")){
-         Array.prototype.push.apply(allChars, alphaLower);
+        Array.apply (allChars, alphaLower);
      }
 
      if(confirm("Would you like your password to contain numbers?")){
-         Array.prototype.push.apply(allChars, number);
+        Array.apply (allChars, number);
      }
 
      if(confirm("Would you like your password to contain symbols?")){
-         Array.prototype.push.apply(allChars, symbol);
+        Array.apply (allChars, symbol);
      }
 
      if(allChars.length===0){
